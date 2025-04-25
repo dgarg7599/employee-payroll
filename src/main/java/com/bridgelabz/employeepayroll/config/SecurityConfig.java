884cfd25676f5a56e8936a1aead93232abd85d72
dgarg7599 <gargdivyansh7599@gmail.com>
2025-04-25 09:24:50 +0530

    [Divyansh Garg] ADD: Created Employee Payroll Application.

--- src/main/java/com/bridgelabz/employeepayroll/config/SecurityConfig.java
+++ src/main/java/com/bridgelabz/employeepayroll/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.bridgelabz.employeepayroll.security.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
